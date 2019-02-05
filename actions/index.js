0d880052f1ff83a2491de16270a2f398abd40787
Svitlana Lesiv <lesivsvitlana@gmail.com>
2019-02-04 21:42:08 -0800

    feat: Add actions

---
+++ actions/index.js
@@ -0,0 +1,44 @@
+import { fetchDecks, saveDeck } from "../utils/api";
+
+export const RECEIVE_DECKS = "RECEIVE_DECKS";
+export const ADD_DECK = "ADD_DECK";
+export const ADD_CARD = "ADD_CARD";
+
+export function receiveDecks(decks) {
+  return {
+    type: RECEIVE_DECKS,
+    decks,
+  }
+}
+
+export function addDeck(deck) {
+  return {
+    type: ADD_DECK,
+    deck,
+  }
+}
+
+export function addCard(card) {
+    return {
+      type: ADD_CARD,
+      card,
+    }
+}
+
+export function handleFetchDecks() {
+    return (dispatch) => {
+      return fetchDecks()
+        .then(({ decks }) => {
+          dispatch(receiveDecks(decks))     
+        })
+    }
+}
+
+export function handleAddDeck(deck, key) {
+    return (dispatch) => {
+       return saveDeck({
+        deck,
+        key
+      }).then((deck) => dispatch(addDeck(deck))
+    )}
+  }
