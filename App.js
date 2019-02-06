e8f8dfad33c2d1c67433c5dd932e99658162721b
Svitlana Lesiv <lesivsvitlana@gmail.com>
2019-02-06 15:42:22 -0800

    feat: Add middleware

--- App.js
+++ App.js
@@ -2,13 +2,16 @@ import React from "react";
 import { createStore } from "redux";
 import { Provider } from "react-redux";
 import reducer from "./reducers";
+import middleware from "./middleware";
 import { View, Text } from "react-native";
+
+const store = createStore(reducer, middleware);
 
 export default class App extends React.Component {
   render() {
     return (
-      <Provider store={createStore(reducer)}>
-        <View style={{flex: 1}}>
+      <Provider store={store}>
+        <View>
           <Text>This is app</Text> 
         </View>
       </Provider>
--- reducers/index.js
+++ reducers/index.js
@@ -1,20 +1,20 @@
 import { RECEIVE_DECKS, ADD_DECK } from "../actions";
 
-export function decks (state = {}, action) {
+export default function decks(state = {}, action) {
 	switch (action.type) {
 		case RECEIVE_DECKS :
 			const { decks } = action;
 			return {
 				...state,
-				...decks,
+				...decks
 			}
 		case ADD_DECK :
 			const { deck } = action;
 			return {
 				...state,
-				[deck.id]: deck,
+				...deck
 			}
-		default:
+		default :
 			return state
 	}
 }
