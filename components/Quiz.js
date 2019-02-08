1042c2895bcc2d82af718d9a4e6e78e37789e0cb
Svitlana Lesiv <lesivsvitlana@gmail.com>
2019-02-07 19:12:35 -0800

    feat: Iterate through questions

--- components/Quiz.js
+++ components/Quiz.js
@@ -1,12 +1,50 @@
 import React, { Component } from "react";
-import { View, Text } from "react-native";
+import { connect } from "react-redux";
+import { View, Text, TouchableOpacity } from "react-native";
 
-export default class Quiz extends Component {
-    render() {
-      return (
-        <View>
-            <Text>This is a quiz</Text> 
-        </View> 
-      );
+class Quiz extends Component {
+  state = {
+    index: 0
+  }
+  static navigationOptions = {
+    title: 'Quiz',
+  };
+
+  nextCard = () => {
+    if(this.state.index < this.props.deck.questions.length-1) {
+      this.setState({
+        index: this.state.index + 1
+      })
     }
+  }
+
+  render() {
+    const { index } = this.state;
+    const { deck } = this.props;
+
+    return (
+      <View>
+        <Text>statistic</Text> 
+        <Text>{deck.questions[index].question}</Text>
+        <Text>{deck.questions[index].answer}</Text>
+        <TouchableOpacity onPress={() => this.nextCard()}>
+          <Text>Correct</Text>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={() => this.nextCard()}>
+          <Text>Incorrect</Text>
+        </TouchableOpacity>
+      </View>
+    );
+  }
 }
+
+function mapStateToProps(state, { navigation }) {
+  const { deckId } = navigation.state.params;
+
+  return {
+    deckId,
+    deck: state[deckId]
+  };
+}
+
+export default connect(mapStateToProps)(Quiz);
