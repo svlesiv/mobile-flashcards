166273ca0ef2e0a73133665c697f12dfa648da15
Svitlana Lesiv <lesivsvitlana@gmail.com>
2019-02-08 16:23:27 -0800

    feat: Add new deck

--- components/Deck.js
+++ components/Deck.js
@@ -1,16 +1,35 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
-import { View, Text } from "react-native";
+import { View, Text, TouchableOpacity, Alert } from "react-native";
 
 class Deck extends Component {
+  static navigationOptions = ({ navigation }) => {
+    const { deckId } = navigation.state.params;
+    return {
+      title: deckId
+    };
+  };
+
+  handleQuizRedirect = () => {
+    this.props.deck.questions.length > 0 
+      ? this.props.navigation.navigate('Quiz', {deckId: this.props.deck.title})
+      : Alert.alert(
+        'This deck doesn\'t have cards'
+      )
+  }
+
   render() {
-    const { deckId } = this.props;
+    const { deck, navigation } = this.props;
     return (
       <View>
-          <Text>{deckId}</Text>
-          <Text>question</Text>
-          <Text>button</Text>
-          <Text>button</Text>
+        <Text>{deck.title}</Text>
+        <Text>{deck.questions.length} cards</Text>
+        <TouchableOpacity onPress={() => navigation.navigate('NewCard', {deckId: deck.title})}>
+          <Text>Add Card</Text>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={() => this.handleQuizRedirect()}>
+          <Text>Quiz</Text>
+        </TouchableOpacity>
       </View> 
     );
   }
@@ -20,7 +39,8 @@ function mapStateToProps(state, { navigation }) {
   const { deckId } = navigation.state.params;
 
   return {
-    deckId
+    deckId,
+    deck: state[deckId]
   };
 }
 
--- components/NewDeck.js
+++ components/NewDeck.js
@@ -1,12 +1,51 @@
 import React, { Component } from "react";
-import { View, Text } from "react-native";
+import { connect } from 'react-redux'
+import { View, Text, TextInput, TouchableOpacity } from "react-native";
+import { addDeck } from "../actions";
+import { saveDeck } from "../utils/api";
 
-export default class NewDeck extends Component {
-    render() {
-      return (
-        <View>
-            <Text>This is a new deck</Text> 
-        </View> 
-      );
-    }
+
+class NewDeck extends Component {
+  state = {
+    text: ''
+  }
+
+  submit = () => {
+    const newDeck = {
+      [this.state.text]: {
+        title: this.state.text,
+        questions: []
+      }
+    };
+    //dispatch action
+    this.props.dispatch(
+      addDeck(
+        newDeck
+      )
+    );
+    //save to AsyncStorage
+    saveDeck(this.state.text, newDeck );
+
+    this.setState({
+      text: ''
+    })
+  }
+  
+  render() {
+    return (
+      <View>
+        <Text>What is the title of your new deck?</Text>
+        <TextInput 
+          onChangeText={(text) => this.setState({text})}
+          value={this.state.text}
+          maxLength = {40}
+          placeholder="Deck Title"/>
+        <TouchableOpacity onPress={() => this.submit()}>
+          <Text>Submit</Text>
+        </TouchableOpacity>
+      </View>
+    );
+  }
 }
+
+export default connect()(NewDeck)
