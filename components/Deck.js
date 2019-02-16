d39fb0f58a9445432a6beaecd7e2cdb9b8e9df54
Svitlana Lesiv <lesivsvitlana@gmail.com>
2019-02-15 18:48:54 -0800

    feat: Add animation

--- components/Deck.js
+++ components/Deck.js
@@ -1,10 +1,24 @@
-import React, { Component } from "react";
-import { connect } from "react-redux";
-import { View, Text, Alert, StyleSheet } from "react-native";
-import Button from "./Button";
-import { textColorPrimary, textColorSecondary, baseColorAccentPrimary } from "../utils/colors"
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { View, Text, Alert, StyleSheet, Animated } from 'react-native';
+import Button from './Button';
+import { textColorPrimary, textColorSecondary, baseColorAccentPrimary } from '../utils/colors';
 
 class Deck extends Component {
+  state = {
+    opacity: new Animated.Value(0),
+    bounceValue: new Animated.Value(0.98)
+  }
+
+  componentDidMount() {
+    const { opacity, bounceValue } = this.state;
+    Animated.sequence([
+      Animated.timing(opacity, { toValue: 1, duration: 200 }),
+      Animated.timing(bounceValue, { duration: 200, toValue: 1.01 }),
+      Animated.timing(bounceValue, { duration: 200, toValue: 1 })
+    ]).start();
+  }
+
   static navigationOptions = ({ navigation }) => {
     const { deckId } = navigation.state.params;
     return {
@@ -13,7 +27,7 @@ class Deck extends Component {
   };
 
   handleQuizRedirect = () => {
-    this.props.deck.questions.length > 0 
+    this.props.deck.questions.length > 0
       ? this.props.navigation.navigate('Quiz', {deckId: this.props.deck.title})
       : Alert.alert(
         'This deck doesn\'t have cards'
@@ -22,21 +36,23 @@ class Deck extends Component {
 
   render() {
     const { deck, navigation } = this.props;
+    const { opacity, bounceValue } = this.state;
+
     return (
-      <View style={styles.container}>
+      <Animated.View style={[styles.container, { opacity, transform: [{scale: bounceValue}] }]}>
         <Text style={styles.header}>{deck.title}</Text>
         <Text style={styles.body}>{deck.questions.length} cards</Text>
         <View style={styles.buttonGroup}>
-          <Button style={{ backgroundColor: baseColorAccentPrimary}} 
+          <Button style={{ backgroundColor: baseColorAccentPrimary}}
                   onPress={() => navigation.navigate('NewCard', {deckId: deck.title})}>
             Add Card
           </Button>
-          <Button style={{ backgroundColor: baseColorAccentPrimary}} 
+          <Button style={{ backgroundColor: baseColorAccentPrimary}}
                   onPress={() => this.handleQuizRedirect()}>
             Start Quiz
           </Button>
         </View>
-      </View> 
+      </Animated.View>
     );
   }
 }
@@ -73,6 +89,6 @@ const styles = StyleSheet.create({
     flexDirection: "row",
     justifyContent: "center",
   }
-})
+});
 
 export default connect(mapStateToProps)(Deck);
